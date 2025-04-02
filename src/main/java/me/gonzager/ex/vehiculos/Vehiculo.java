f6dd72bc4b6dd4c1bb0a3a519efbc8b3372e790d
Gonzalez <B060018@servexternos.santandertecnologia.com.ar>
2025-04-02 10:22:13 -0300

    clean para la practica

--- src/main/java/me/gonzager/ex/vehiculos/Vehiculo.java
+++ src/main/java/me/gonzager/ex/vehiculos/Vehiculo.java
@@ -1,53 +1,5 @@
 package me.gonzager.ex.vehiculos;
 
-import me.gonzager.ex.tiposconduccion.Ecologica;
-import me.gonzager.ex.tiposconduccion.TipoConduccion;
-
 public class Vehiculo {
-    private Double combustible;
-    private Double kilometros = 0.0;
-    private TipoConduccion tipoConduccion = new Ecologica();
-
-    public Vehiculo(Double combustible) {
-        if (combustible < 0) {
-            throw new RuntimeException("La cantidad de combustible no puede ser negativa.");
-        }
-        this.combustible = combustible;
-    }
-
-    public void avanzarTipoConduccion() {
-        this.tipoConduccion = this.tipoConduccion.siguiente();
-    }
-
-    public void retrocederTipoConduccion() {
-        this.tipoConduccion = this.tipoConduccion.anterior();
-    }
-
-    public void desplazar(Double cantKms) {
-        Double combustibleNecesario = (1 / this.tipoConduccion.consumo()) * cantKms;
-        Double kilometrosPosibles = this.combustible * this.tipoConduccion.consumo();
-        this.kilometros += Double.min(cantKms, kilometrosPosibles);
-        this.combustible = Double.max(0.0, this.combustible - combustibleNecesario);
-
-        if (cantKms > kilometrosPosibles)
-            throw new RuntimeException("Combustible insuficiente, solo pude recorrer " + kilometrosPosibles
-                    + " del total de " + cantKms + " kilometros.");
-    }
-
-    public Double getCombustible() {
-        return combustible;
-    }
-
-    public Double getKilomentros() {
-        return kilometros;
-    }
-
-    public Double velodadMaxima() {
-        return this.tipoConduccion.velocidadMaxima();
-    }
-
-    public Double consumo() {
-        return this.tipoConduccion.consumo();
-    }
 
 }
--- src/test/java/me/gonzager/ex/vehiculos/VehiculoTest.java
+++ src/test/java/me/gonzager/ex/vehiculos/VehiculoTest.java
@@ -9,71 +9,28 @@ public class VehiculoTest {
     @Test
     void vehículoRetrocediendoYAvanzandoTiposDeConduccionDeberíaRetornarCorrectamenteLaVelocidad() {
 
-        var vehiculo = new Vehiculo(3.0);
-
-        vehiculo.retrocederTipoConduccion(); // Se queda en Ecologica
-        vehiculo.avanzarTipoConduccion(); // Lo avanzo a Estandar
-        vehiculo.avanzarTipoConduccion(); // Lo avanzo a Deportiva
-        vehiculo.avanzarTipoConduccion(); // Se queda en Deportiva
-        vehiculo.retrocederTipoConduccion(); // retrocede a Estandar
-
-        assertEquals(150, vehiculo.velodadMaxima());
+        assertEquals(150, 150);
     }
 
     @Test
     void vehículoEnConduccionEstandarDeberíaAvanzarCorrectamenteYReducirElCombustible() {
 
-        var vehiculo = new Vehiculo(25.0);
-
-        vehiculo.avanzarTipoConduccion(); // Lo avanzo a Estandar
-        vehiculo.desplazar(200.0);
-
-        assertEquals(200.0, vehiculo.getKilomentros());
-        assertEquals(5.0, vehiculo.getCombustible());
     }
 
     @Test
     void vehiculoEnConduccionDeportivaDeberíaLanzarUnaExcepcionSiNoHaySuficienteCombustible() {
 
-        var vehiculo = new Vehiculo(13.0);
-
-        vehiculo.avanzarTipoConduccion();
-        vehiculo.avanzarTipoConduccion();
-
-        assertThrows(
-                RuntimeException.class,
-                () -> {
-                    vehiculo.desplazar(75.0);
-                },
-                "Combustible insuficiente, solo pude recorrer 65 del total de 75.0 kilometros.");
-        assertEquals(65.0, vehiculo.getKilomentros());
-        assertEquals(0.0, vehiculo.getCombustible());
+        assertEquals(0.0, 0.0);
     }
 
     @Test
     void valorDeCombustibleMenorACeroAlCrearVehiculoLanzaExcepcion() {
 
-        assertThrows(
-                RuntimeException.class,
-                () -> {
-                    new Vehiculo(-10.0);
-                },
-                "La cantidad de combustible no puede ser negativa.");
     }
 
     @Test
     void velocidadMaximaDiferenteSegunTipoDeConduccion() {
 
-        var vehiculo = new Vehiculo(10.0);
-
-        var velocidadEcologica = vehiculo.velodadMaxima();
-        vehiculo.avanzarTipoConduccion(); // avanza a estandar
-        var velocidadEstandar = vehiculo.velodadMaxima();
-        vehiculo.avanzarTipoConduccion(); // avanza a deportiva
-        var velocidadDeportiva = vehiculo.velodadMaxima();
-
-        assertTrue(velocidadEcologica < velocidadEstandar);
-        assertFalse(velocidadEstandar > velocidadDeportiva);
-        assertNotEquals(velocidadEstandar, velocidadDeportiva);
+        assertNotEquals(1, 0);
     }
 }
